c55009445d94ca70eb02c00cd34c3f452a487b6a
tocka <tocka.ayman@virgingates.com>
2021-08-18 09:11:12 +0200

    Set default controller url

--- src/main/java/com/workmotion/employees/controllers/EmployeeController.java
+++ src/main/java/com/workmotion/employees/controllers/EmployeeController.java
@@ -7,6 +7,7 @@ import com.workmotion.employees.services.EmployeeServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/api/v1/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -15,23 +16,22 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    //TODO: lift url to controller level
-    @PostMapping("/api/v1/employees")
+    @PostMapping
     public Employee create(@RequestBody Employee employee) {
         return employeeService.create(employee);
     }
 
-    @PostMapping("/api/v1/employees/{id}/state-event")
+    @PostMapping("/{id}/state-event")
     public Employee sendEvent(@PathVariable String id, @RequestBody EmployeeEventDTO eventDTO) throws Exception {
         return employeeService.sendEventStateMachine(id, eventDTO.getEvent());
     }
 
-    @PostMapping("/api/v1/employees/{id}/state-event/kafka")
+    @PostMapping("/{id}/state-event/kafka")
     public Employee sendEventKafka(@PathVariable String id, @RequestBody EmployeeEventDTO eventDTO) throws Exception {
         return employeeService.sendEventKafka(id, eventDTO.getEvent());
     }
 
-    @GetMapping("/api/v1/employees/{id}")
+    @GetMapping("/{id}")
     public Employee getEmployee(@PathVariable String id) throws Exception {
         return employeeService.getEmployee(id);
     }
